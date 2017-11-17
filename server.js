5a31d56182d32f3635f2cbeb1388371547c070ae
Matthew Nagy <grndprxguy@gmail.com>
2017-11-16 19:26:09 -0500

    table array

--- server.js
+++ server.js
@@ -3,15 +3,13 @@ var bodyParser = require("body-parser");
 var path = require("path");
 
 //Express set up port etc...
-
+var app = express();
+var PORT = process.env.PORT || 8080;
 //express set up body parser...
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 //Array of tables
-
-
-
-//function to determine table or waitlist
+var tableArray = [];
 
 //function to determine position in reservation or waitlist
 
